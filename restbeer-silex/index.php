f4c455175d0f0a6d6d4c8fbe01858e1ae3ce07ea
lucas <lucas_vn95@hotmail.com>
2015-05-29 17:07:52 -0300

    primeiro commit

--- restbeer-silex/index.php
+++ restbeer-silex/index.php
@@ -14,38 +14,39 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/views',
 ));
 
-$cervejas = array(
-    'marcas' => array('Heineken', 'Guinness', 'Skol', 'Colorado'),
-    'estilos' => array('Pilsen' , 'Stout')
-);
 
-$app->get('/estilo', function () use ($cervejas, $app) {
-    return new Response(implode(',', $cervejas['estilos']), 200);
+// $app->get('/estilo', function () use ($db, $app) {
+//     return new Response(implode(',', $cervejas['estilos']), 200);
+// });
+
+$app->get('/cerveja', function () use ($db, $app) {
+    $stmt = $db->prepare('select name,style from beer');
+    $stmt->execute();
+    $cervejas = $stmt->fetchAll();
+
+    return new Response(print_r($cervejas), 200);
+
 });
 
-$app->get('/cerveja/{id}', function ($id) use ($cervejas, $app) {
-    if ($id == null) {
-       return new Response(implode(',', $cervejas['marcas']), 200);
-    }
-    
-    $key = array_search($id, $cervejas['marcas']);
-    if (!$key) {
-        return new Response('Não encontrado', 404);
-    }
-    return new Response($cervejas['marcas'][$key], 200);
+$app->get('/cerveja/{id}', function ($id) use ($db, $app) {
+    $stmt = $db->prepare('select name,style from beer where id=:id');
+    $stmt->bindParam(':id',$id);
+    $stmt->execute();
+    $cervejas = $stmt->fetchAll();
 
+    return new Response(print_r($cervejas), 200);
 })->value('id', null);
 
 $app->post('/cerveja', function (Request $request) use ($app, $db) {
-	$db->exec(
-		"create table if not exists beer (id INTEGER PRIMARY KEY AUTOINCREMENT, name text not null, style text not null)"
-	);
-	if (!$request->get('name') || !$request->get('style')) {
+    $db->exec(
+        "create table if not exists beer (id INTEGER PRIMARY KEY AUTOINCREMENT, name text not null, style text not null)"
+    );
+    if (!$request->get('name') || !$request->get('style')) {
         return new Response('Faltam parâmetros', 400);
     }
     $cerveja = [
-    	'name'  => $request->get('name'),
-    	'style' => $request->get('style')
+        'name'  => $request->get('name'),
+        'style' => $request->get('style')
     ];
     
     $stmt = $db->prepare('insert into beer (name, style) values (:name, :style)');
@@ -59,11 +60,29 @@ $app->post('/cerveja', function (Request $request) use ($app, $db) {
 });
 
 $app->put('/cerveja/{id}', function (Request $request, $id) use ($app) {
-    
+    if (!$request->get('name')) {
+        return new Response('Faltam parâmetros', 400);
+    }
+    $cerveja = [
+        'name'  => $request->get('name'),
+        'id'  => $id,
+    ];
+
+    $stmt = $db->prepare('update beer set name=:name where id=:id');
+    $stmt->bindParam(':name',$cerveja['name']);
+    $stmt->bindParam(':id',$cerveja['id']);
+    $stmt->execute();
+
+    return new Response("Cerveja {$cerveja['id']} - {$cerveja['name']} alterada com sucesso!", 200);
 });
 
 $app->delete('/cerveja/{id}', function (Request $request, $id) use ($app) {
-   
+
+    $stmt = $db->prepare('delete from beer where id=:id');
+    $stmt->bindParam(':id',$id);
+    $stmt->execute();
+
+    return new Response("Cerveja {$id} deletada com sucesso!", 200);
 });
 
 
@@ -79,19 +98,19 @@ $app->before(function (Request $request) use ($app) {
 });
 
 $app->after(function (Request $request, Response $response) use ($app) {
-	$content = explode(',', $response->getContent());
+    $content = explode(',', $response->getContent());
 
-	if ($request->headers->get('accept') == 'text/json') {
-		$response->headers->set('Content-Type', 'text/json');
-		$response->setContent(json_encode($content));
-	}
+    if ($request->headers->get('accept') == 'text/json') {
+        $response->headers->set('Content-Type', 'text/json');
+        $response->setContent(json_encode($content));
+    }
 
-	if ($request->headers->get('accept') == 'text/html') {
-		$content = $app['twig']->render('content.twig', array('content' => $content));
-		$response->setContent($content);
-	}
+    if ($request->headers->get('accept') == 'text/html') {
+        $content = $app['twig']->render('content.twig', array('content' => $content));
+        $response->setContent($content);
+    }
 
-	return $response;
+    return $response;
 });
 
 
